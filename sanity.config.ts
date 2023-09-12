89c40fffdbfd0a625c91eadbd54f884a6caa4325
mercedeswalsh <mercedes.scott@live.com>
2023-09-12 18:26:04 -0500

    fixed errors

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "sanity"
-import { deskTool } from "sanity/desk";
-// import schemas from "./sanity/schemas"
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import schemas from "./sanity/schemas"
 
 const config = defineConfig({
     projectId: "7rg63h57",
@@ -9,7 +9,7 @@ const config = defineConfig({
     apiVersion: "2023-09-11",
     basePath: "/admin",
     plugins: [deskTool()],
-    // schema: { types: schemas }
+    schema: { types: schemas }
 })
 
-export default config
+export default config;
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "7rg63h57",
+  dataset: "production",
+  apiVersion: "2023-09-11",
+}
+
+export default config;
